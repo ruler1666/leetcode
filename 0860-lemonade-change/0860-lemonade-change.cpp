1e1a2f50136714fc1a4ab45d56f9726f23d4de5b
ruler1666 <aditya.nav06@gmail.com>
2026-09-03 11:46:01 +0530

    Time: 1 ms (14.59%), Space: 87.4 MB (10.97%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5)mp[5]++;
+
+            if(bills[i]==10){
+                if(mp[5]!=0){
+                    mp[5]--;
+                    mp[10]++;
+                }else{
+                    return false;
+                }
+            }
+
+            if(bills[i]==20){
+                if(mp[10]>=1 && mp[5]>=1){
+                    mp[10]--;
+                    mp[5]--;
+                    mp[20]++;
+                }else if(mp[5]>=3){
+                    mp[5]--;
+                    mp[5]--;
+                    mp[5]--;
+
+                    mp[20]++;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
